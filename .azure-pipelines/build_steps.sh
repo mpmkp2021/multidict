66f33de30859a29f0d3de4ae7cee569690b623ec
Dish1306 <60138605+Dish1306@users.noreply.github.com>
2020-07-23 15:37:30 +0530

    Update build_steps.sh

--- .azure-pipelines/build_steps.sh
+++ .azure-pipelines/build_steps.sh
@@ -15,37 +15,20 @@ conda-build:
  root-dir: ${FEEDSTOCK_ROOT}/build_artifacts
 CONDARC
 
-#conda install --yes --quiet conda-forge-ci-setup=2 conda-build -c conda-forge
-
-# set up the condarc
-#setup_conda_rc "${FEEDSTOCK_ROOT}" "${RECIPE_ROOT}" "${CONFIG_FILE}"
-
-#source run_conda_forge_build_setup
-
-# make the build number clobber
-#make_build_number "${FEEDSTOCK_ROOT}" "${RECIPE_ROOT}" "${CONFIG_FILE}"
-
-#conda build "${RECIPE_ROOT}" -m "${CI_SUPPORT}/${CONFIG}.yaml" \
-#    --clobber-file "${CI_SUPPORT}/clobber_${CONFIG}.yaml"
-
-#if [[ "${UPLOAD_PACKAGES}" != "False" ]]; then
-#    upload_package "${FEEDSTOCK_ROOT}" "${RECIPE_ROOT}" "${CONFIG_FILE}"
-#fi
-
-#touch "${FEEDSTOCK_ROOT}/build_artifacts/conda-forge-build-done-${CONFIG}"
 sudo yum update -y
 sudo yum install gcc gcc-c++ python3-devel wget -y
 cd '/home/conda/feedstock_root'
 export CONDA_ENV='travisci'
-echo "Installing archiconda"
-#${FEEDSTOCK_ROOT}/buildscripts/incremental/setup_conda_environment.sh
-#export PATH='/opt/conda/bin':${PATH}
-#echo "Setting up Conda environment"
-#${FEEDSTOCK_ROOT}/.azure-pipelines/setup_conda_environment.sh
-#echo "Building multidict"
-#source deactivate
-#${FEEDSTOCK_ROOT}/.azure-pipelines/build.sh
-#conda install -y flake8
-#flake8 numba
-#echo "Testing numba"
-#${FEEDSTOCK_ROOT}/.azure-pipelines/test.h
+echo "Installing requirement"
+pip install -r requirements/lint.txt
+make flake8
+pip install -e .
+export MULTIDICT_NO_EXTENSIONS= 1
+make mypy
+yum install libenchant-dev
+pip install -r requirements/doc-spelling.txt
+pip install -r requirements/towncrier.txt
+towncrier --yes
+pip install -U twine wheel
+python setup.py sdist bdist_wheel
+twine check dist/*

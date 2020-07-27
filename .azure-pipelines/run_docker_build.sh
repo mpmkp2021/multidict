6f316411f526184e7e640802c433df6294b5863a
Dish1306 <60138605+Dish1306@users.noreply.github.com>
2020-07-27 13:16:59 +0530

    Update run_docker_build.sh

--- .azure-pipelines/run_docker_build.sh
+++ .azure-pipelines/run_docker_build.sh
@@ -37,8 +37,7 @@ docker run ${DOCKER_RUN_ARGS} \
            -e HOST_USER_ID \
            -e CI \
            $DOCKER_IMAGE \
-           ls 
-           #bash .azure-pipelines/build_steps.sh
+           bash /home/conda/feedstock_root/.azure-pipelines/build_steps.sh
 
 # verify that the end of the script was reached
 test -f "$DONE_CANARY"

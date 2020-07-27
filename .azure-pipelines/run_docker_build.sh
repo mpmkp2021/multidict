f5c7bc64e225e762705a0f2e21714f6ab7ed70f0
Dish1306 <60138605+Dish1306@users.noreply.github.com>
2020-07-27 13:03:49 +0530

    Update run_docker_build.sh

--- .azure-pipelines/run_docker_build.sh
+++ .azure-pipelines/run_docker_build.sh
@@ -37,7 +37,8 @@ docker run ${DOCKER_RUN_ARGS} \
            -e HOST_USER_ID \
            -e CI \
            $DOCKER_IMAGE \
-           bash .azure-pipelines/build_steps.sh
+           ls
+           #bash .azure-pipelines/build_steps.sh
 
 # verify that the end of the script was reached
 test -f "$DONE_CANARY"
